a7d24caa41bdfd49b2a796855ab8a071b893a018
Roland Grunberg <rgrunber@redhat.com>
2014-07-12 17:06:50 -0400

    Improve simpleSingleUnitInstallTest to also check the profile.
    After the single IU is installed, a check is done using the
    SimpleArtifactRepository Mapper logic to determine if installation was
    successfull. In addition we should also check the generated profile for
    the IU.

--- org.fedoraproject.p2.tests/src/org/fedoraproject/p2/tests/InstallTest.java
+++ org.fedoraproject.p2.tests/src/org/fedoraproject/p2/tests/InstallTest.java
@@ -15,6 +15,12 @@ import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -48,25 +54,69 @@ public class InstallTest extends RepositoryTest {
 				}
 			}
 
+			final String profileID = "testProfile";
+			final String installLoc = ResourcesPlugin.getWorkspace().getRoot().getLocation().toOSString()
+					+ File.separator + getClass().getName();
+
 			// TODO: Fix test to create the profile and use the proper InstallOperation API
 			if (targetIU != null) {
 				String args [] = new String [] {"-repository", JAVADIR,
 						"-installIU", targetIU.getId(),
-						"-destination", ResourcesPlugin.getWorkspace().getRoot().getLocation().toOSString()
-						+ File.separator + getClass().getName()};
+						"-profile", profileID,
+						"-destination", installLoc};
 				DirectorApplication app = new DirectorApplication();
 				app.run(args);
 
 				// See org.eclipse.equinox.internal.p2.artifact.repository.simple.Mapper
 				String fileName = targetIU.getId() + "_" + targetIU.getVersion() + ".jar";
-				assertTrue(new File(ResourcesPlugin.getWorkspace().getRoot().getLocation().toOSString()
-						+ File.separator + getClass().getName() + File.separator + "plugins" + File.separator + fileName).exists());
+				assertTrue(new File(installLoc + File.separator + "plugins" + File.separator + fileName).exists());
 
+				File [] profileFiles = getProfileFiles(new File(installLoc));
+
+				// Get the latest '.profile.gz' file
+				File latestProfileFile = null;
+				long timestamp = 0;
+				for (File file : profileFiles) {
+					long ts = Long.parseLong(file.getName().replace(".profile.gz", ""));
+					if (ts > timestamp) {
+						timestamp = ts;
+						latestProfileFile = file;
+					}
+				}
+
+				// A profile with ID of ${profileID} must be contained in a folder ${profileID}.profile
+				String profileDir = File.separator + "tmp" + File.separator + profileID + ".profile";
+				new File(profileDir).mkdir();
+				String profileFile = profileDir + File.separator + latestProfileFile.getName();
+				Files.copy(Paths.get(latestProfileFile.getAbsolutePath()), Paths.get(profileFile), StandardCopyOption.REPLACE_EXISTING);
+
+				IMetadataRepository profile = getMetadataRepoManager().loadRepository(new URI("file:" + profileFile), new NullProgressMonitor());
+				IQueryResult<IInstallableUnit> profRes = profile.query(QueryUtil.createIUAnyQuery(), new NullProgressMonitor());
+				Set<IInstallableUnit> profUnits = profRes.toUnmodifiableSet();
+
+				assertTrue(profUnits.size() == 1);
+				assertTrue(profUnits.contains(targetIU));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
 		}
+	}
+
+	private File[] getProfileFiles(File root) {
+		List<File> res = new ArrayList<File>();
+		for (File child : root.listFiles()) {
+			File [] tmp = null;
+			if (child.isDirectory() && child.canRead()) {
+				tmp = getProfileFiles(child);
+			} else if (child.isFile() && child.getName().endsWith(".profile.gz")) {
+				tmp = new File [] {child};
+			}
+			if (tmp != null) {
+				res.addAll(Arrays.asList(tmp));
+			}
+		}
+		return res.toArray(new File[0]);
 	}
 
 }
